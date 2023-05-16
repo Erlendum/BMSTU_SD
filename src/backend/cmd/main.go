baa088429cdb615772f109bb26e2bd63a5bffa32
Erlendum <erlendum.ds@gmail.com>
2023-05-16 15:05:21 +0300

    switch mode in backend was added.

--- src/backend/cmd/main.go
+++ src/backend/cmd/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"backend/cmd/console"
 	"backend/cmd/modes"
+	"flag"
 )
 
-func main() {
-	app := modes.App{}
-	//app := console.App{}
+var mode = flag.String("mode", "http-server", "mode of backend")
 
-	app.Run()
+func main() {
+	flag.Parse()
+
+	switch *mode {
+	case "cli":
+		app := console.App{}
+		app.Run()
+	case "http-server":
+		app := modes.App{}
+		app.Run()
+	}
 }
